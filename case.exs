9bf63aec175a150a780f41489c1eeb1b01c483a4
Alexandr N <naberezhnyy.an@gmail.com>
2022-12-29 14:56:13 +0100

    add unless

--- case.exs
+++ case.exs
@@ -10,18 +10,21 @@ end
 
 # output : "qwe 1"
 
-case {1,2,3} do
-  {1,2,4} ->
+case {1, 2, 3} do
+  {1, 2, 4} ->
     IO.puts("case 1")
-  {1,x,3} ->
+
+  {1, x, 3} ->
     IO.puts("qwe 2")
 end
+
 # output : "qwe 2"
 
 y = 2
 
-case {1,2,3} do
-  {1,^y,3} ->
+case {1, 2, 3} do
+  {1, ^y, 3} ->
     IO.puts("qwe 3")
 end
+
 # output : "qwe 3"
---
+++ unless.exs
@@ -0,0 +1,7 @@
+a =
+  unless 1 == 2 do
+    IO.puts("case 1")
+  end
+
+# output: "case 2"
+IO.puts("a : #{a}")
