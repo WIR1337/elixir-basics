f1278b57c7d8f89fce2dc5cfe07214da223c5c18
Alexandr N <naberezhnyy.an@gmail.com>
2023-01-01 14:11:11 +0100

    add charlist

---
+++ charlist.exs
@@ -0,0 +1,8 @@
+# A charlist is a list of integers where all the integers are valid code points.
+IO.puts('hełło')
+
+# <> binary operator
+# ++ list concatenation operator
+
+# 'he' <> 'llo' error
+# 'he' ++ 'llo' correct
